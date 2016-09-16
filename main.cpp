ff9721ccf3f7211858f846f27003526c50dc26f2
Kirill Osipatov <antoniokarpov@gmail.com>
2016-09-16 12:04:12 +0400

    3.1 s komentariem

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+//programma yavlyaetsya uchebnoi
 int main()
 {
    int a;
    int b;
    int c;
+   int s;
     cout << "Vvedite dva chisla" << endl;
     cin >> a;
     cin >> b;
+    s=a+b;
     c=a-b;
+    cout <<s << endl;
     cout <<c << endl;
     return 0;
 }
